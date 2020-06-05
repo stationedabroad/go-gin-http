7dad7d6f641a47eaf77ede4513583f4363b66e7a
sulman shaikh <sulmanmh@gmail.com>
2020-06-05 23:31:49 +0100

    implemented video save func

--- service/video-service.go
+++ service/video-service.go
@@ -17,6 +17,11 @@ func New() VideoService {
 	return &videoService{}
 }
 
+func (service *videoService) Save(video entity.Video) entity.Video {
+	service.videos = append(service.videos, video)
+	return video
+}
+
 func (service *videoService) FindAll() []entity.Video {
 	return service.videos
 }
