c2de3c35ef200e5f2d3174fc37a04881fc04e254
sulman shaikh <sulmanmh@gmail.com>
2020-06-06 18:09:11 +0100

    main server end points added for find and save

--- server.go
+++ server.go
@@ -1,16 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stationedabroad/go-gin-http/controller"
+	"github.com/stationedabroad/go-gin-http/service"
+)
 
-var port string = ":8080"
+var (
+	port            string                     = ":8080"
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "Server started OK!",
-		})
+	server.GET("/posts", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/save", func(ctx *gin.Context) {
+		ctx.JSON(201, videoController.Save(ctx))
 	})
 
 	server.Run(port)
