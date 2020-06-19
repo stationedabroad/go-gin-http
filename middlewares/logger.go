246a8cbb8435fcea26001a2c207d09aab7449bce
sulman shaikh <sulmanmh@gmail.com>
2020-06-19 23:36:13 +0100

    logger signatue added for customer log formatter

---
+++ middlewares/logger.go
@@ -0,0 +1,11 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Logger() gin.HandlerFunc {
+	return gin.LoggerWithFormatter(func(params gin.LogFormatterParams) string {
+		return "dummylogger"
+	})
+}
