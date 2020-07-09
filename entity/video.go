f906a0ee69442a79ea97c84c93a90420cc02e16a
sulman shaikh <sulmanmh@gmail.com>
2020-07-09 23:24:55 +0100

    validation on author entity changed: lte=110

--- entity/video.go
+++ entity/video.go
@@ -3,7 +3,7 @@ package entity
 type Individual struct {
 	FirstName string `json:"firstname" binding:"required"`
 	LastName  string `json:"lastname" binding:"required"`
-	Age       int8   `json:"age"  binding:"gte=1,lte=130"`
+	Age       int8   `json:"age"  binding:"gte=1,lte=110"`
 	Email     string `json:"email" validate:"required,email"`
 }
 
